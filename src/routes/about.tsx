df1bf0f35bf13ac90659570b930a3060d6956827
Jorge <jbarretolarrosa@gmail.com>
2025-05-09 21:47:29 -0300

    initial tailwindcss with shadcn config

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/about")({
@@ -5,5 +6,10 @@ export const Route = createFileRoute("/about")({
 });
 
 function About() {
-    return <div className="p-2">Hello from About!</div>;
+    return (
+        <div className="p-2 bg-red-500">
+            Hello from About!
+            <Button variant={"destructive"}>Apretame</Button>
+        </div>
+    );
 }
