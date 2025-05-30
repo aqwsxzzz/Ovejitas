b81b0dd6c4b76114b8e1fcf605f60228f40a5388
Jorge <jbarretolarrosa@gmail.com>
2025-05-30 17:15:23 -0300

    feat: switch

---
+++ src/features/auth/sign-up-form.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { z } from "zod";
+
+const SignUpForm = () => {
+    return <div></div>;
+};
+
+export default SignUpForm;
