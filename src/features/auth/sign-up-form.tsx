88981064ee56532b14158ecc340362e676e42656
Jorge <jbarretolarrosa@gmail.com>
2025-05-30 17:25:30 -0300

    feat: switch

--- src/features/auth/sign-up-form.tsx
+++ src/features/auth/sign-up-form.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { z } from "zod";
+//import { Button } from "@/components/ui/button";
+//import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+//import { z } from "zod";
 
 const SignUpForm = () => {
     return <div></div>;
