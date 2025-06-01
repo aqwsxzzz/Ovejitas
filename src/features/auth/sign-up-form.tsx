a0c2aee40603c973359fa9196c650ca3211517ee
Jorge <jbarretolarrosa@gmail.com>
2025-06-01 20:36:48 -0300

    feat(sign-up-card): add email control input

--- src/features/auth/sign-up-form.tsx
+++ src/features/auth/sign-up-form.tsx
@@ -1,9 +1,50 @@
 //import { Button } from "@/components/ui/button";
-//import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-//import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {zodResolver} from "@hookform/resolvers/zod"
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    email: z.string().email(),
+    name: z.string(),
+    password: z.string().min(6),
+})
 
 const SignUpForm = () => {
-    return <div></div>;
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {email: '', name: '', password: '',}
+    });
+
+
+
+    return <div>
+        <div>
+            <p>Create your account</p>
+            <Form {...form}>
+                <form>
+                    <FormField
+                    control={form.control}
+                    name="email"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input 
+                                placeholder="Enter your email"
+                                {...field}/>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}>
+
+                    </FormField>
+                </form>
+            </Form>
+        </div>
+    </div>;
 };
 
 export default SignUpForm;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import SignUpForm from "@/features/auth/sign-up-form";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -8,6 +9,7 @@ function Index() {
     return (
         <div className="p-2">
             <h3>Welcome Home!</h3>
+            <SignUpForm />
         </div>
     );
 }
