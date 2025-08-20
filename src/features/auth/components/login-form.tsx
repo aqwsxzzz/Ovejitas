02d71475521f9c2fd1dbd344ea701352ed475192
Jorge <jbarretolarrosa@gmail.com>
2025-08-20 15:51:49 -0300

    feat(auth): enhance login form with loading state and improve code formatting

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,64 +1,86 @@
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { useLogin } from "@/features/auth/api/auth-queries";
 import { useTranslation } from "react-i18next";
+import { Loader } from "lucide-react";
 
 const formSchema = z.object({
-    email: z.string().email(),
-    password: z.string(),
+	email: z.string().email(),
+	password: z.string(),
 });
 
 export const LoginForm = () => {
-    const { mutateAsync: login } = useLogin();
+	const { mutateAsync: login, isPending } = useLogin();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: { email: "", password: "" },
-    });
-    const onSubmit = async (data: z.infer<typeof formSchema>) => {
-        await login({ payload: data });
-    };
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: { email: "", password: "" },
+	});
+	const onSubmit = async (data: z.infer<typeof formSchema>) => {
+		await login({ payload: data });
+	};
 
-    const password = form.watch("password");
+	const password = form.watch("password");
 
-    const { t } = useTranslation("login");
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-7">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>{t("emailInputLabel")}</FormLabel>
-                            <FormControl>
-                                <Input placeholder={t("emailPlaceholder")} {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>{t("passwordInputLabel")}</FormLabel>
-                            <FormControl>
-                                <Input autoComplete="new-password" placeholder={t("passwordPlaceholder")} {...field} type="password" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit" disabled={password.length < 8}>
-                    {t("loginButton")}
-                </Button>
-            </form>
-        </Form>
-    );
+	const { t } = useTranslation("login");
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="flex flex-col gap-7"
+			>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>{t("emailInputLabel")}</FormLabel>
+							<FormControl>
+								<Input
+									placeholder={t("emailPlaceholder")}
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>{t("passwordInputLabel")}</FormLabel>
+							<FormControl>
+								<Input
+									autoComplete="new-password"
+									placeholder={t("passwordPlaceholder")}
+									{...field}
+									type="password"
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button
+					type="submit"
+					disabled={password.length < 8}
+				>
+					{isPending ? <Loader className="spinner" /> : t("loginButton")}
+				</Button>
+			</form>
+		</Form>
+	);
 };
