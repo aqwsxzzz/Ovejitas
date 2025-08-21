312a9416f053885efab71f4efc39701958107a14
Jorge <jbarretolarrosa@gmail.com>
2025-08-21 20:18:09 -0300

    refactor(axios-helper): improve code formatting and consistency

--- src/lib/axios/axios-helper.ts
+++ src/lib/axios/axios-helper.ts
@@ -2,34 +2,38 @@ import { axiosInstance } from "@/lib/axios";
 import { AxiosError, type AxiosResponse } from "axios";
 
 export const axiosHelper = async <Res, Params = unknown, Data = unknown>({
-    method,
-    url,
-    urlParams,
-    data,
-    headers,
+	method,
+	url,
+	urlParams,
+	data,
+	headers,
 }: {
-    method: "get" | "post" | "put" | "delete" | "patch";
-    url: string;
-    urlParams?: Params;
-    data?: Data;
-    headers?: {
-        Authorization?: `Bearer ${string}`;
-    };
+	method: "get" | "post" | "put" | "delete" | "patch";
+	url: string;
+	urlParams?: Params;
+	data?: Data;
+	headers?: {
+		Authorization?: `Bearer ${string}`;
+	};
 }): Promise<Res> => {
-    return axiosInstance[method]<Res>(url, method === "get" ? { params: urlParams, headers } : data, { headers })
-        .then((res: AxiosResponse<Res>) => {
-            if (!res || !res.data) {
-                throw new Error("No response or response data is undefined");
-            }
+	return axiosInstance[method]<Res>(
+		url,
+		method === "get" ? { params: urlParams, headers } : data,
+		{ headers },
+	)
+		.then((res: AxiosResponse<Res>) => {
+			if (!res || !res.data) {
+				throw new Error("No response or response data is undefined");
+			}
 
-            return res.data as Res;
-        })
-        .catch((error: unknown) => {
-            if (error instanceof AxiosError) {
-                if (error.response) {
-                    throw error.response.data.data;
-                }
-            }
-            throw error;
-        });
+			return res.data as Res;
+		})
+		.catch((error: unknown) => {
+			if (error instanceof AxiosError) {
+				if (error.response) {
+					throw error.response.data;
+				}
+			}
+			throw error;
+		});
 };
