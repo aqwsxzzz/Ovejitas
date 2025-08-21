631c219e8849db7d5994a6abf779981a6e6fc852
Jorge <jbarretolarrosa@gmail.com>
2025-08-21 19:39:44 -0300

    feat(signup): ispending state now have a spinner to show

--- src/features/auth/components/sign-up-form.tsx
+++ src/features/auth/components/sign-up-form.tsx
@@ -1,76 +1,104 @@
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { useSignUp } from "@/features/auth/api/auth-queries";
 import { useTranslation } from "react-i18next";
+import { Loader } from "lucide-react";
 
 const formSchema = z.object({
-    email: z.string().email(),
-    displayName: z.string().min(2, "Name must have at least 2 characters."),
-    password: z.string().min(8, "Password must have at least 8 characters."),
+	email: z.string().email(),
+	displayName: z.string().min(2, "Name must have at least 2 characters."),
+	password: z.string().min(8, "Password must have at least 8 characters."),
 });
 
 export const SignUpForm = () => {
-    const { mutateAsync: signUp, error } = useSignUp();
+	const { mutateAsync: signUp, error, isPending } = useSignUp();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: { email: "", displayName: "", password: "" },
-    });
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: { email: "", displayName: "", password: "" },
+	});
 
-    const onSubmit = async (data: z.infer<typeof formSchema>) => {
-        await signUp({ payload: data });
-    };
+	const onSubmit = async (data: z.infer<typeof formSchema>) => {
+		await signUp({ payload: data });
+	};
 
-    const { t } = useTranslation("signup");
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-7">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>{t("emailInputLabel")}</FormLabel>
-                            <FormControl>
-                                <Input placeholder={t("emailPlaceholder")} {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="displayName"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>{t("displayNameInputLabel")}</FormLabel>
-                            <FormControl>
-                                <Input placeholder={t("displayNamePlaceholder")} {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>{t("passwordInputLabel")}</FormLabel>
-                            <FormControl>
-                                <Input autoComplete="new-password" placeholder={t("passwordPlaceholder")} {...field} type="password" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                {error && <p className="text-destructive">{error.message}</p>}
-                <Button type="submit">{t("createAccountButton")}</Button>
-            </form>
-        </Form>
-    );
+	const { t } = useTranslation("signup");
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="flex flex-col gap-7"
+			>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>{t("emailInputLabel")}</FormLabel>
+							<FormControl>
+								<Input
+									placeholder={t("emailPlaceholder")}
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="displayName"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>{t("displayNameInputLabel")}</FormLabel>
+							<FormControl>
+								<Input
+									placeholder={t("displayNamePlaceholder")}
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>{t("passwordInputLabel")}</FormLabel>
+							<FormControl>
+								<Input
+									autoComplete="new-password"
+									placeholder={t("passwordPlaceholder")}
+									{...field}
+									type="password"
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{error && <p className="text-destructive">{error.message}</p>}
+				<Button type="submit">
+					{isPending ? (
+						<Loader className="animate-spin" />
+					) : (
+						t("createAccountButton")
+					)}
+				</Button>
+			</form>
+		</Form>
+	);
 };
